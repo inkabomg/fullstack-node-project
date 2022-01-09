739757049eff4e0d409682e4c0a6ef315a7bb047
Inka Lagerbom <77788881+inkabomg@users.noreply.github.com>
2022-01-09 22:07:14 +0200

    Create User (view) component

---
+++ frontend/src/components/User.js
@@ -0,0 +1,22 @@
+import React from "react";
+// import './Home.css';
+
+function UserComponent() {
+
+    return (
+    <div>
+
+    <form>
+      <>
+        <input
+          label="WORD"
+          placeholder='Input translation..'
+          name='text'
+          ></input>
+      </>
+    </form>
+    </div>
+  );
+}
+
+export default UserComponent;
