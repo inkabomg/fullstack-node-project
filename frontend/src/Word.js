ead51ff42c7873967a318837105ce927e3559861
Inka Lagerbom <77788881+inkabomg@users.noreply.github.com>
2022-01-13 22:37:30 +0200

    Fix DELETE request button

--- frontend/src/Word.js
+++ frontend/src/Word.js
@@ -8,8 +8,15 @@ function Word({
   word: { id, tag, english, finnish },
   captureEdit,
   changeEditState,
-  deleteWord,
 }) {
+  //const [state, setState] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("http://localhost:8080/vocabulary/")
+  //     .then((resp) => resp.json())
+  //     .then((data) => setState(data)); // set data to state
+  // }, []);
+
   return (
     <TableRow key={id}>
       {/* // The <td> HTML element defines a cell of a table that contains data */}
@@ -35,7 +42,7 @@ function Word({
           variant="contained"
           size="small"
           style={{ marginTop: "5px", background: "#a9d4c7" }}
-          onClick={() => deleteWord(word.id)}
+          onClick={() => deleteWord(id)}
         >
           Delete
         </Button>
