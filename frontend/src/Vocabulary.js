f53c1116376adf1945018a5bcb9928451c04e8dc
Inka Lagerbom <77788881+inkabomg@users.noreply.github.com>
2022-01-13 16:27:08 +0200

    Add mui components

--- frontend/src/Vocabulary.js
+++ frontend/src/Vocabulary.js
@@ -1,13 +1,13 @@
-import React, {useState, useEffect} from 'react'
-import Word from './Word'
-import EditWord from './EditWord'
-import "./Vocabulary.css"
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+import React, { useState, useEffect } from "react";
+import Word from "./Word";
+import EditWord from "./EditWord";
+import "./Vocabulary.css";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
 
 function Vocabulary({ vocabulary, onUpdateWord }) {
   // state for conditional render of edit form
@@ -17,113 +17,127 @@ function Vocabulary({ vocabulary, onUpdateWord }) {
   const [editForm, setEditForm] = useState({
     tag: "",
     english: "",
-    finnish: ""
+    finnish: "",
   });
 
   useEffect(() => {
     fetch("http://localhost:8080/vocabulary/")
-      .then(resp => resp.json())
-      .then(data => setState(data)) // set data to state
+      .then((resp) => resp.json())
+      .then((data) => setState(data)); // set data to state
   }, []);
 
   // when PATCH request happens; auto-hides the form, pushes changes to display
   function handleWordUpdate(updatedWord) {
     setIsEditing(false);
     onUpdateWord(updatedWord);
-    console.log(updatedWord)
-  };
+    console.log(updatedWord);
+  }
 
   // capture user input in edit form inputs
   function handleTagChange(e) {
     setEditForm({
-    ...editForm,
-    tag: e.target.value
-    })
-  };
+      ...editForm,
+      tag: e.target.value,
+    });
+  }
 
   function handleEngChange(e) {
     setEditForm({
-    ...editForm,
-    english: e.target.value
-    })
-  };
+      ...editForm,
+      english: e.target.value,
+    });
+  }
 
   function handleFinChange(e) {
     setEditForm({
-    ...editForm,
-    finnish: e.target.value
-    })
-  };
+      ...editForm,
+      finnish: e.target.value,
+    });
+  }
 
   // needed logic for conditional rendering of the form - shows the word you want when you want them, and hides it when you don't
   function changeEditState(word) {
     if (word.id === editForm.id) {
-      setIsEditing(isEditing => !isEditing) // hides the form
+      setIsEditing((isEditing) => !isEditing); // hides the form
     } else if (isEditing === false) {
-      setIsEditing(editing => !editing) // shows the form
+      setIsEditing((editing) => !editing); // shows the form
     }
-    console.log(word)
+    console.log(word);
   }
 
-// capture the word you wish to edit, set to state
+  // capture the word you wish to edit, set to state
   function captureEdit(clickedWord) {
-    let filtered = vocabulary.filter(word => word.id === clickedWord.id)
-    setEditForm(filtered[0])
+    let filtered = vocabulary.filter((word) => word.id === clickedWord.id);
+    setEditForm(filtered[0]);
   }
 
   function deleteWord(id) {
-    const removed = [...state].filter(word => word.id !== id);
+    const removed = [...state].filter((word) => word.id !== id);
     setState(removed);
     fetch(`http://localhost:8080/vocabulary/` + id, {
       method: "DELETE",
       headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-    })
-  };
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+  }
 
-  // // create a tableinto which the data gets pushed
+  // // Create a table into which the data gets pushed
   return (
     <div>
-      {isEditing?
-          (<EditWord
-            editForm={editForm}
-            handleTagChange={handleTagChange}
-            handleEngChange={handleEngChange}
-            handleFinChange={handleFinChange}
-            handleWordUpdate={handleWordUpdate}
-          />) : null}
-    <TableContainer>
-      <Table sx={{ maxWidth: 450 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Tag</TableCell>
-            <TableCell>English</TableCell>
-            <TableCell>Finnish</TableCell>
-            <TableCell>Modify word</TableCell>
-            <TableCell>Delete word</TableCell>
-          </TableRow>
-        </TableHead>
-          <TableBody>
-          {/* iterate through the vocabulary array and render a unique
-          Word component for each word object in the array */}
-          { vocabulary.map(word =>
-            <Word
-              key={word.id}
-              word={word}
-              captureEdit={captureEdit}
-              changeEditState={changeEditState}
-              deleteWord={deleteWord}
-            />)}
+      {isEditing ? (
+        <EditWord
+          editForm={editForm}
+          handleTagChange={handleTagChange}
+          handleEngChange={handleEngChange}
+          handleFinChange={handleFinChange}
+          handleWordUpdate={handleWordUpdate}
+        />
+      ) : null}
+      <TableContainer>
+        <Table sx={{ maxWidth: 450 }} aria-label="simple table" size="small">
+          <TableHead>
             <TableRow>
+              <TableCell style={{ background: "#ceede4" }}>Tag</TableCell>
+              <TableCell style={{ background: "#ceede4" }}>English</TableCell>
+              <TableCell style={{ background: "#ceede4" }}>Finnish</TableCell>
+              <TableCell
+                sx={{ minWidth: 90 }}
+                style={{ background: "#ceede4" }}
+              >
+                Modify word
+              </TableCell>
+              <TableCell
+                sx={{ minWidth: 90 }}
+                style={{ background: "#ceede4" }}
+              >
+                Delete word
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* iterate through the vocabulary array and render a unique
+          Word component for each word object in the array */}
+            {vocabulary.map((word) => (
+              <Word
+                key={word.id}
+                word={word}
+                captureEdit={captureEdit}
+                changeEditState={changeEditState}
+                deleteWord={deleteWord}
+              />
+            ))}
+            <TableRow
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
               <TableCell>{vocabulary.english}</TableCell>
               <TableCell>{vocabulary.finnish}</TableCell>
             </TableRow>
-        </TableBody>
-      </Table>
+          </TableBody>
+        </Table>
       </TableContainer>
-      </div>
+    </div>
   );
 }
 
