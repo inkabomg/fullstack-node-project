7f390e81a3d830fc30425d860accc5735b3c8917
Inka Lagerbom <77788881+inkabomg@users.noreply.github.com>
2022-01-13 22:38:43 +0200

    Remove PUT request

--- frontend/src/EditWord.js
+++ frontend/src/EditWord.js
@@ -1,36 +1,60 @@
-import React from 'react'
-import Button from '@mui/material/Button';
+import React from "react";
+import Button from "@mui/material/Button";
 
-function EditWord({ editForm, handleWordUpdate, updatedWord, handleTagChange, handleEngChange, handleFinChange }) {
-    let { id, tag, english, finnish } = editForm
+function EditWord({
+  editForm,
+  handleWordUpdate,
+  updatedWord,
+  handleTagChange,
+  handleEngChange,
+  handleFinChange,
+}) {
+  let { tag, english, finnish } = editForm;
 
-// PATCH/PUT (??? which one works better) request; calls handleWordUpdate to push changes to the page
-    function handleEditForm(e) {
-        e.preventDefault();
-        fetch(`http://localhost:8080/vocabulary/${id}`, {
-            method: "PATCH",
-            headers: {
-                "Content-Type" : "application/json"
-            },
-            body: JSON.stringify(editForm),
-        })
-            .then(resp => resp.json())
-            .then(updatedWord => {
-                handleWordUpdate(updatedWord)
-            })
-    }
+  function handleEditForm(e) {
+    e.preventDefault();
+    handleWordUpdate();
+  }
 
-    return (
-        <div>
-            <h4>Edit word(s)</h4>
-            <form onSubmit={handleEditForm}>
-                <input placeholder="Tag" type="text" name="tag" value={tag} onChange={handleTagChange}/>
-                <input placeholder="Word in English" type="text" name="english" value={english} onChange={handleEngChange}/>
-                <input placeholder="Word in Finnish" type="text" name="finnish" value={finnish} onChange={handleFinChange}/>
-                <Button variant="contained" size="small" style={{marginLeft:"5px", background:"#a9d4c7"}}onClick={() => {handleWordUpdate(updatedWord)}} type="submit">Submit changes</Button>
-            </form>
-            <br></br>
-        </div>
-    )
+  return (
+    <div>
+      <h4>Edit word(s)</h4>
+      <form onSubmit={handleEditForm}>
+        <input
+          placeholder="Tag"
+          type="text"
+          name="tag"
+          value={tag}
+          onChange={handleTagChange}
+        />
+        <input
+          placeholder="Word in English"
+          type="text"
+          name="english"
+          value={english}
+          onChange={handleEngChange}
+        />
+        <input
+          placeholder="Word in Finnish"
+          type="text"
+          name="finnish"
+          value={finnish}
+          onChange={handleFinChange}
+        />
+        <Button
+          variant="contained"
+          size="small"
+          style={{ marginLeft: "5px", background: "#a9d4c7" }}
+          onClick={() => {
+            handleWordUpdate(updatedWord);
+          }}
+          type="submit"
+        >
+          Submit changes
+        </Button>
+      </form>
+      <br></br>
+    </div>
+  );
 }
 export default EditWord;
