70671359869351821b2f53f0dbf5e911ed0b544e
Inka Lagerbom <77788881+inkabomg@users.noreply.github.com>
2022-01-13 22:42:37 +0200

    Add comments

--- frontend/src/Table.js
+++ frontend/src/Table.js
@@ -1,27 +1,37 @@
 import React from "react";
-import TableCell from '@mui/material/TableCell';
-import TableRow from '@mui/material/TableRow';
-import Input from '@mui/material/Input';
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import Input from "@mui/material/Input";
 
 function TableFin(props) {
-
-    function checkInput(e) {
-        const input = e.target.value
-        //
-        const correctInput = input.toLowerCase();
-        if (correctInput === props.finnish) {
-            props.correctInput(correctInput)
-        } else {
-            props.incorrectInput(correctInput)
-        }
+  function checkInput(e) {
+    const input = e.target.value;
+    // transform all input to lowercase to match
+    const correctInput = input.toLowerCase();
+    // compare input to the finnish word
+    if (correctInput === props.finnish) {
+      props.correctInput(correctInput);
+    } else {
+      props.incorrectInput(correctInput);
     }
-    return (
-        <TableRow>
-            <TableCell>{props.english}</TableCell>
-            <TableCell>
-                <Input placeholder="Type in Finnish" type="text" onBlur={checkInput}/></TableCell>
-        </TableRow>
-    )
+  }
+
+  return (
+    <TableRow>
+      {/* // display words on the left in English */}
+      <TableCell>{props.english}</TableCell>
+      <TableCell>
+        <Input
+          sx={{ maxWidth: 180, fontSize: 11 }}
+          placeholder="Type in Finnish"
+          type="text"
+          size="small"
+          // onblur event occurs when an object loses focus
+          onBlur={checkInput}
+        />
+      </TableCell>
+    </TableRow>
+  );
 }
 
 export default TableFin;
