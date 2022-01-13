8b3be2ffaa9e113e417af8f1b892360acbf9fcac
Inka Lagerbom <77788881+inkabomg@users.noreply.github.com>
2022-01-13 16:27:53 +0200

    Add score display

--- frontend/src/User.js
+++ frontend/src/User.js
@@ -1,28 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TableEng from "./TableEng";
 import TableFin from "./Table";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Button from '@mui/material/Button';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
 
-function UserComponent() {
+var point = 0;
+
+function UserComponent(props) {
   const [state, setState] = useState([]);
   const [title, setTitle] = useState("");
-  var [point, setPoint] = useState(0);
+  const [finalScore, setFinalScore] = useState([]);
 
   // useEffect(() => {
-  //   (async () => {
-  //     const result = await fetch("http://localhost:8080/vocabulary/");
-  //     setState(result.data);
-  //   })();
+  //   const fetchTag = async () => {
+  //     const result = await fetch(
+  //       `http://localhost:8080/vocabulary/${props.tag}`
+  //     );
+  //     let json = Object.values(result.data);
+  //     setState(json);
+  //   };
+  //   fetchTag();
   // }, []);
 
   async function getFinnish() {
-    const result = await fetch("http://localhost:8080/vocabulary/")
+    const result = await fetch("http://localhost:8080/vocabulary/");
     const state = await result.json();
     const table = state.map((id) => {
       return (
@@ -33,21 +40,25 @@ function UserComponent() {
           correctInput={correctInput}
           incorrectInput={incorrectInput}
         />
-      )
+      );
     });
     setState(table);
     setTitle(
       <TableHead>
         <TableRow>
-          <TableCell>Finnish</TableCell>
-          <TableCell>English</TableCell>
+          <TableCell style={{ background: "#ceede4", fontWeight: "bold" }}>
+            Finnish
+          </TableCell>
+          <TableCell style={{ background: "#ceede4", fontWeight: "bold" }}>
+            English
+          </TableCell>
         </TableRow>
       </TableHead>
-    )
+    );
   }
 
   async function getEnglish() {
-    const result = await fetch("http://localhost:8080/vocabulary/")
+    const result = await fetch(`http://localhost:8080/vocabulary/`);
     const state = await result.json();
     const table = state.map((id) => {
       return (
@@ -58,51 +69,94 @@ function UserComponent() {
           correctInput={correctInput}
           incorrectInput={incorrectInput}
         />
-      )
+      );
     });
     setState(table);
     setTitle(
       <TableHead>
         <TableRow>
-          <TableCell>English</TableCell>
-          <TableCell>Finnish</TableCell>
+          <TableCell style={{ background: "#ceede4", fontWeight: "bold" }}>
+            English
+          </TableCell>
+          <TableCell style={{ background: "#ceede4", fontWeight: "bold" }}>
+            Finnish
+          </TableCell>
         </TableRow>
       </TableHead>
-    )
+    );
   }
 
   function correctInput(a) {
     point++;
     console.log(a);
-    console.log("Your score is: " + point)
+    console.log("Your score is: " + point);
   }
 
-  function incorrectInput(a) {
-    console.log("Wrong answer")
+  function incorrectInput() {
+    console.log("Wrong answer");
   }
 
-  function checkResults() {
-
+  function showScore() {
+    if (point === state.length[1]) {
+      setFinalScore(
+        <Alert style={{ maxWidth: 450, margin: "auto" }} severity="success">
+          You got all the words correct!
+        </Alert>
+      );
+    } else if (point === 0 && state.length) {
+      setFinalScore(
+        <Alert style={{ maxWidth: 450, margin: "auto" }} severity="error">
+          You didn't get one word correct!
+        </Alert>
+      );
+    } else {
+      setFinalScore("You scored " + point + "/" + state.length);
+    }
   }
-
 
   return (
     <div>
       <h2>Learn Finnish!</h2>
-      <Button onClick={getEnglish} variant="contained" size="small" style={{marginBottom:"20px", marginRight:"10px", background:"#a9d4c7"}}>Show words in English</Button>
-      <Button onClick={getFinnish} variant="contained" size="small" style={{marginBottom:"20px", background:"#a9d4c7"}}>Show words in Finnish</Button>
+      <Button
+        onClick={getEnglish}
+        variant="contained"
+        size="small"
+        style={{
+          marginBottom: "20px",
+          marginRight: "10px",
+          background: "#a9d4c7",
+        }}
+      >
+        Show words in English
+      </Button>
+      <Button
+        onClick={getFinnish}
+        variant="contained"
+        size="small"
+        style={{ marginBottom: "20px", background: "#a9d4c7" }}
+      >
+        Show words in Finnish
+      </Button>
       <div className="list">
         <TableContainer>
-          <Table sx={{ maxWidth: 450 }}>
+          <Table sx={{ maxWidth: 450 }} size="small">
             {title}
             <TableBody>{state}</TableBody>
           </Table>
         </TableContainer>
-        <Button variant="contained" size="small" style={{marginTop:"20px", background:"#a9d4c7"}}>Check results</Button>
+        <Button
+          onClick={showScore}
+          variant="contained"
+          size="small"
+          style={{ marginTop: "20px", background: "#a9d4c7" }}
+        >
+          Check results
+        </Button>
       </div>
+      <br></br>
+      {finalScore}
     </div>
   );
-
 }
 
 export default UserComponent;
