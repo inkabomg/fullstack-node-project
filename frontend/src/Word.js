27b55d67a477b25f83bb0aacbeda46b58fbed587
Inka Lagerbom <77788881+inkabomg@users.noreply.github.com>
2022-01-14 13:32:20 +0200

    Remove comments

--- frontend/src/Word.js
+++ frontend/src/Word.js
@@ -10,21 +10,12 @@ function Word({
   changeEditState,
   deleteWord,
 }) {
-  //const [state, setState] = useState([]);
-
-  // useEffect(() => {
-  //   fetch("http://localhost:8080/vocabulary/")
-  //     .then((resp) => resp.json())
-  //     .then((data) => setState(data)); // set data to state
-  // }, []);
-
   return (
     <TableRow key={id}>
-      {/* // The <td> HTML element defines a cell of a table that contains data */}
+      {/* // The element defines a cell of a table that contains data */}
       <TableCell>{tag}</TableCell>
       <TableCell>{english}</TableCell>
       <TableCell>{finnish}</TableCell>
-      {/* // TODO: edit and delete */}
       <TableCell>
         <Button
           variant="contained"
